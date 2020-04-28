dc09e179711b136c71c7b1ca2e027e9304d1c3a9
imraghav20 <raghav20nov@gmail.com>
2020-04-28 20:15:15 +0530

    created frontend for interface and post fragment

--- app/src/main/java/com/example/iitjapp/InterfaceFragment.java
+++ app/src/main/java/com/example/iitjapp/InterfaceFragment.java
@@ -4,15 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class InterfaceFragment extends Fragment {
+
+    private ImageButton suggestions, invitation, books, testpapers;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_interface, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_interface, container, false);
+
+        suggestions = (ImageButton) rootView.findViewById(R.id.suggestions);
+        invitation = (ImageButton) rootView.findViewById(R.id.invitation);
+        books = (ImageButton) rootView.findViewById(R.id.books);
+        testpapers = (ImageButton) rootView.findViewById(R.id.testpapers);
+
+        return rootView;
     }
 }
--- app/src/main/java/com/example/iitjapp/PostFragment.java
+++ app/src/main/java/com/example/iitjapp/PostFragment.java
@@ -4,15 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class PostFragment extends Fragment {
+
+    private ImageButton buy_and_sell, gallery;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_post, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_post, container, false);
+
+        buy_and_sell = (ImageButton) rootView.findViewById(R.id.buy_and_sell);
+        gallery = (ImageButton) rootView.findViewById(R.id.gallery);
+
+        return rootView;
     }
 }
